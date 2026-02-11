dbbaaacbb2864465f20aaa2a28877a74cade6daa
Anabelt-hub <anabelt@clemson.edu>
2026-02-10 22:15:12 -0500

    Update app.js

--- collection_manager/app.js
+++ collection_manager/app.js
@@ -9,7 +9,7 @@
  * - Local dev (Flask): http://localhost:5000
  * - Deployed backend:  https://your-backend-host.com
  */
-const API_BASE = "https://collection-api.onrender.com";
+const API_BASE = "https://solo-2-back.onrender.com";
 
 const el = (id) => document.getElementById(id);
 
@@ -391,4 +391,5 @@ if (document.readyState === "loading") {
 } else {
   init();
 }
+
 
